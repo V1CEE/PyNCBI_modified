7085f3c903f2d73455cc027cbb20ee7d2ecb29bc
Shaked Weis <98441009+V1CEE@users.noreply.github.com>
2024-01-28 21:28:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,8 +16,7 @@ setup(
   long_description=read_md('README.md'),
   author = 'Thomas Konstantinovsky',
   author_email = 'thomaskon90@gmail.com',
-  url = 'https://github.com/MuteJester/PyNCBI',
-  download_url = 'https://github.com/MuteJester/PyNCBI/archive/refs/tags/0.1.6.1.tar.gz',
+  url = 'https://github.com/V1CEE/PyNCBI_modified',
   keywords = ['api','data database','analytics,biology','methylation','epigenetics','ncbi','epigenetic-data'],   # Keywords that define your package best
     install_requires=[
         'methylprep',
@@ -33,5 +32,6 @@ setup(
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10'
   ],
 )
